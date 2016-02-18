02aa660527401c1454d3b9843833857d8e753635
PaladinTyrion <paladintyrion@gmail.com>
2016-02-19 01:02:23 +0800

    skiplist seek/insert/delete

--- skiplist/level.go
+++ skiplist/level.go
@@ -6,18 +6,17 @@ import (
 )
 
 const (
-	p = 0.75
+	p = 0.5
 )
 
 // generator is seeded with unix nanosecond and only executed once
 // ensuring all random numbers come from the same randomly seeded generator.
 var generator = rand.New(rand.NewSource(time.Now().UnixNano()))
 
-var glLock = sync.Mutex
+var glLock sync.Mutex
 
 // generatorLevel generator a threadsafe level with a range from 0 to MaxLevel.
 func generatorLevel(maxlevel uint8) (level uint8) {
-	var level uint8
 	glLock.Lock()
 	defer glLock.Unlock()
 
--- skiplist/skiplist.go
+++ skiplist/skiplist.go
@@ -5,10 +5,6 @@ type SkipList struct {
 	level 		uint8
 	head        *node
 	length      uint64
-
-	// In the insert/delete case, cache record the pre-Node
-	// before operation at every level.
-	cache		nodes
 }
 
 func (sl *SkipList) init(para interface{}) {
@@ -24,7 +20,6 @@ func (sl *SkipList) init(para interface{}) {
 		default:
 		sl.maxLevel = 32
 	}
-	sl.cache = make(nodes, sl.maxLevel)
 	sl.head = newNode(nil, sl.maxLevel)
 }
 
@@ -39,22 +34,113 @@ func (sl *SkipList) Len() uint64 {
 	return sl.length
 }
 
-func (s *SkipList) seach(val Comparator) (cache *node) {
-	return
+func (sl *SkipList) search(val Comparator, cache nodes) (resNode *node) {
+	if sl.length == 0 {
+		return nil
+	}
+
+	n := sl.head
+	var alreadyChecked *node
+	for i := int(sl.level)-1; i >= 0 ; i-- {
+		for ;n.forward[i] != nil && n.forward[i] != alreadyChecked && n.forward[i].Compare(val) < 0; {
+			n = n.forward[i]
+		}
+
+		alreadyChecked = n
+		if cache != nil {
+			cache[i] = n
+		}
+	}
+	return n.forward[0]
 }
 
-func (s *SkipList) Insert(val Comparator) (overwrite Comparator) {
-	return
+func (sl *SkipList) Seek(vals ...Comparator) (results Comparators) {
+	results = make(Comparators, 0, len(vals))
+
+	for _, val := range vals {
+		n := sl.search(val, nil)
+		if n != nil && n.Compare(val) == 0 {
+			results = append(results, n.entry)
+		}
+	}
+	return results
+}
+
+func (sl *SkipList) insert(n *node, val Comparator, cache nodes, allowOverwrite bool) (overWrite Comparator) {
+	if !allowOverwrite && n != nil && n.entry.Compare(val) == 0 {
+		return val
+	}
+
+	nLevel := generatorLevel(sl.maxLevel)
+	if nLevel > sl.level {
+		for i := sl.level; i < nLevel; i++ {
+			cache[i] = sl.head
+		}
+		sl.level = nLevel
+	}
+
+	nn := newNode(val, sl.maxLevel)
+	for i := uint8(0); i < nLevel; i++ {
+		nn.forward[i] = cache[i].forward[i]
+		cache[i].forward[i] = nn
+	}
+
+	sl.length++
+
+	return nil
+}
+
+
+func (sl *SkipList) Insert(val Comparator) (overwrite Comparator) {
+	//cache saves pre-Nodes of searching val in the skiplist at every level
+	cache := make(nodes, sl.maxLevel)
+	n := sl.search(val, cache)
+	return sl.insert(n, val, cache, false)
+}
+
+func (sl *SkipList) delete(n *node, val Comparator, cache nodes) (del Comparator) {
+	if n == nil || n.Compare(val) != 0 {
+		return nil
+	}
+
+	sl.length--
+	for i := uint8(0); i < sl.level; i++ {
+		cache[i].forward[i] = n.forward[i]
+	}
+
+	for sl.level > 1 && sl.head.forward[sl.level-1] == nil {
+		sl.level--
+	}
+
+	return n.entry
 }
 
 func (sl *SkipList) Delete(val Comparator) (del Comparator) {
-	return
+	cache := make(nodes, sl.maxLevel)
+	n := sl.search(val, cache)
+	return sl.delete(n, val, cache)
 }
 
-func (s *SkipList) InsertMul(vals ...Comparators) (overwrite Comparators) {
-	return
+func (sl *SkipList) InsertMul(vals ...Comparator) (overwrites Comparators) {
+	for _, val := range vals {
+		cache := make(nodes, sl.maxLevel)
+		n := sl.search(val, cache)
+		overwrite := sl.insert(n, val, cache, false)
+		if overwrite != nil {
+			overwrites = append(overwrites, overwrite)
+		}
+	}
+	return overwrites
 }
 
-func (sl *SkipList) DeleteMul(vals ...Comparators) (del Comparators) {
-	return
+func (sl *SkipList) DeleteMul(vals ...Comparator) (dels Comparators) {
+	for _, val := range vals {
+		cache := make(nodes, sl.maxLevel)
+		n := sl.search(val, cache)
+		del := sl.delete(n, val, cache)
+		if del != nil {
+			dels = append(dels, del)
+		}
+	}
+	return dels
 }
